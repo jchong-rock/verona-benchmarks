d8b42c2bf6e06ce13a55b8e61b84343aaaecd078
Luke Cheeseman <luke_cheeseman@hotmail.co.uk>
2022-09-02 17:04:37 +0100

    adding benchmarks to run

--- savina/savina.cpp
+++ savina/savina.cpp
@@ -5,11 +5,15 @@
 
 int main(const int argc, const char** argv) {
   BenchmarkHarness savina(argc, argv);
-  // savina.run<ActorBenchmark::Banking, 12>(1000, 50000);
+
+  std::cout << "Running Actor Benchmarks" << std::endl;
+  savina.run<ActorBenchmark::Banking, 12>(1000, 50000);
   // savina.run<ActorBenchmark::SleepingBarber, 12>(5000, 1000, 1000, 1000);
   // savina.run<ActorBenchmark::BndBuffer, 12>(50, 40, 40, 1000, 25, 25);
   // savina.run<ActorBenchmark::Cigsmok, 12>(1000, 200);
   // savina.run<ActorBenchmark::DiningPhilosophers, 12>(20, 10000, 1);
 
-  savina.run<BOCBenchmark::SleepingBarber, 12>(5000, 1000, 1000, 1000);
+  std::cout << "Running BoC Benchmarks" << std::endl;
+  savina.run<BOCBenchmark::Banking, 12>(1000, 50000);
+  // savina.run<BOCBenchmark::SleepingBarber, 12>(5000, 1000, 1000, 1000);
 }
