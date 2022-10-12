b0888099d5c5da8aa0d304d9e0e6356c3a1ef38f
Luke Cheeseman <luke_cheeseman@hotmail.co.uk>
2022-10-12 16:46:58 +0100

    fixing termination bug it recmatmul

--- savina/actors/parallel/recmatmul.h
+++ savina/actors/parallel/recmatmul.h
@@ -24,22 +24,22 @@ struct Master {
   cown_ptr<Collector> collector;
 
   uint64_t sent;
+  uint64_t completed;
   uint64_t num_workers;
 
   Master() {}
 
-  static cown_ptr<Master> make(uint64_t workers, uint64_t data_length, uint64_t threshold);
+  static void make(uint64_t workers, uint64_t data_length, uint64_t threshold);
   void send_work(uint64_t priority, uint64_t srA, uint64_t scA, uint64_t srB, uint64_t scB, uint64_t srC, uint64_t scC, uint64_t length, uint64_t dimension);
   static void work(const cown_ptr<Master>& self, uint64_t priority, uint64_t srA, uint64_t scA, uint64_t srB, uint64_t scB, uint64_t srC, uint64_t scC, uint64_t length, uint64_t dimension);
   static void done(const cown_ptr<Master>&);
 };
 
 struct Collector {
-  cown_ptr<Master> master;
   uint64_t length;
   vector<vector<uint64_t>> result;
 
-  Collector(const cown_ptr<Master>& master, uint64_t length): master(master), length(length) {
+  Collector(uint64_t length): length(length) {
     for (uint64_t i = 0; i < length; ++i)
       result.push_back(vector<uint64_t>(length, 0));
   }
@@ -54,8 +54,6 @@ struct Collector {
 
         self->result[i][j] = r;
       }
-
-      Master::done(self->master);
     };
   }
 
@@ -70,19 +68,20 @@ struct Worker {
   bool did_work;
 
   Worker(cown_ptr<Master> master, cown_ptr<Collector> collector, vector<vector<uint64_t>> matrix_a, vector<vector<uint64_t>> matrix_b, uint64_t threshold)
-    :master(move(master)), collector(move(collector)), matrix_a(matrix_a), matrix_b(matrix_b), threshold(threshold), did_work(false) {}
+    :master(move(master)), collector(move(collector)), matrix_a(move(matrix_a)), matrix_b(move(matrix_b)), threshold(threshold), did_work(false) {}
 
   static void work(const cown_ptr<Worker>& self, uint64_t priority, uint64_t srA, uint64_t scA, uint64_t srB, uint64_t scB, uint64_t srC, uint64_t scC, uint64_t length, uint64_t dimension);
 };
 
-cown_ptr<Master> Master::make(uint64_t workers, uint64_t data_length, uint64_t threshold) {
+void Master::make(uint64_t workers, uint64_t data_length, uint64_t threshold) {
   cown_ptr<Master> master = make_cown<Master>();
-  when(master) << [workers, data_length, threshold, tag=master](acquired_cown<Master> master)  mutable{
+  when(master) << [workers, data_length, threshold, tag=move(master)](acquired_cown<Master> master)  mutable{
     master->length = data_length;
     master->num_blocks = data_length * data_length;
     master->sent = 0;
+    master->completed = 0;
     master->num_workers = workers;
-    master->collector = make_cown<Collector>(tag, data_length);
+    master->collector = make_cown<Collector>(data_length);
 
     vector<vector<uint64_t>> a;
     vector<vector<uint64_t>> b;
@@ -109,7 +108,6 @@ cown_ptr<Master> Master::make(uint64_t workers, uint64_t data_length, uint64_t t
 
     master->send_work(0, 0, 0, 0, 0, 0, 0, master->num_blocks, data_length);
   };
-  return master;
 }
 
 void Master::send_work(uint64_t priority, uint64_t srA, uint64_t scA, uint64_t srB, uint64_t scB, uint64_t srC, uint64_t scC, uint64_t length, uint64_t dimension) {
@@ -126,7 +124,7 @@ void Master::work(const cown_ptr<Master>& self, uint64_t priority, uint64_t srA,
 
 void Master::done(const cown_ptr<Master>& self) {
   when(self) << [](acquired_cown<Master> self)  mutable{
-    if (--self->num_workers == 0) {
+    if (++self->completed == self->sent) {
       self->workers.clear();
       self->collector = nullptr;
       /* done */
@@ -177,6 +175,7 @@ void Worker::work(const cown_ptr<Worker>& self, uint64_t priority, uint64_t srA,
 
       Collector::collect(self->collector, move(partial_result));
     }
+    Master::done(self->master);
   };
 }
 
