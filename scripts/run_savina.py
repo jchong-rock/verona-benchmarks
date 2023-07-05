cf79833088b18d98b7915ce35ebbfa74b1e1977f
Matthew Parkinson <mattpark@microsoft.com>
2023-07-05 16:13:50 +0100

    Provide arguments to locate binaries.

--- scripts/run_savina.py
+++ scripts/run_savina.py
@@ -6,12 +6,16 @@ import argparse
 
 # This will measure setup and teardown time
 
+verona_path = "."
+pony_path = "."
 
 def getopts():
     parser = argparse.ArgumentParser(description='Run throughput test.')
     parser.add_argument('--repeats', type=int, default=30,
                         help='number of times to repeat the runs')
     parser.add_argument('-o', default='output', help='output directory location')
+    parser.add_argument('--verona-path', default='.', help='path containing verona executables')
+    parser.add_argument('--pony-path', default='.', help='path containing pony executables')
     args = parser.parse_args()
     return args
 
@@ -23,33 +27,38 @@ def run_test(args, filename):
 def run_boc_actor(cores, output_directory, repeats):
     print(f"Running boc (actor) on {cores} core")
     # run the boc actor benchmark
-    run_test(["./savina", "--actor", "--csv", "--cores", f'{cores}', "--reps", f'{repeats}'], output_directory + f"/boc_actor{cores}.csv")
+    run_test([verona_path + f"/savina", "--actor", "--csv", "--cores", f'{cores}', "--reps", f'{repeats}'], output_directory + f"/boc_actor{cores}.csv")
 
 def run_boc_full(cores, output_directory, repeats):
     print(f"Running boc (full) on {cores} core")
-    run_test(["./savina", "--full", "--csv", "--cores", f'{cores}', "--reps", f'{repeats}'], output_directory + f"/boc_full{cores}.csv")
+    run_test([verona_path + "/savina", "--full", "--csv", "--cores", f'{cores}', "--reps", f'{repeats}'], output_directory + f"/boc_full{cores}.csv")
 
 def run_pony(cores, output_directory, repeats):
     print(f"Running Pony on {cores} core")
     # run the pony benchmark
-    run_test(["./savina-pony", "--parseable", "--ponymaxthreads", f"{cores}", "--reps", f'{repeats}'], output_directory + f"/pony{cores}.csv")
+    run_test([pony_path + "/savina-pony", "--parseable", "--ponymaxthreads", f"{cores}", "--reps", f'{repeats}'], output_directory + f"/pony{cores}.csv")
 
 if __name__ == '__main__':
     args = getopts()
+
+    verona_path = args.verona_path
+    pony_path = args.pony_path
 
     output_directory = args.o
     if not os.path.exists(output_directory):
         os.mkdir(output_directory)
 
-    run_test(["./savina-stats", "--cores", "1", "--reps", "1", "--actor"], output_directory + f"/actor_stats.csv")
-    run_test(["./savina-stats", "--cores", "1", "--reps", "1", "--full"], output_directory + f"/boc_stats.csv")
+    # Calculate stats of the benchmarks (cown/behaviour counts)
+    run_test([verona_path + "/savina-stats", "--cores", "1", "--reps", "1", "--actor"], output_directory + f"/actor_stats.csv")
+    run_test([verona_path + "/savina-stats", "--cores", "1", "--reps", "1", "--full"], output_directory + f"/boc_stats.csv")
 
-    run_test(["./savina", "--scale", "--csv", "--cores", "72", "--reps", "50"], output_directory + f"/banking_scale.csv")
+    # Run the banking scale benchmark
+    run_test([verona_path + "/savina", "--scale", "--csv", "--cores", "72", "--reps", "50"], output_directory + f"/banking_scale.csv")
 
-    run_pony(8, output_directory, args.repeats)
-#    run_pony(4, output_directory, args.repeats)
-#    run_pony(2, output_directory, args.repeats)
     run_pony(1, output_directory, args.repeats)
+#    run_pony(2, output_directory, args.repeats)
+#    run_pony(4, output_directory, args.repeats)
+    run_pony(8, output_directory, args.repeats)
 
     run_boc_full(1, output_directory, args.repeats)
 #    run_boc_full(2, output_directory, args.repeats)
