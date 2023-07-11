53e756b5aab1f673a6ab4ab00b30944285f8e9df
Matthew Parkinson <mattpark@microsoft.com>
2023-07-11 09:44:40 +0100

    Remove results, as moved to artifact repo.

--- scripts/produce_graph_banking_scale.py
+++ scripts/produce_graph_banking_scale.py
@@ -23,49 +23,50 @@ if __name__ == '__main__':
     reader = csv.reader(csvfile, delimiter=',')
     datum = []
 
+    print("Loading data")
     for row in reader:
       paradigm, cores, benchmark, time = row
       datum.append({ "benchmark": benchmark.lower(), "paradigm": paradigm, "cores": cores, "time": float(time) })
 
-      fig = px.box(datum, x="cores", y="time", boxmode="group")
+    print("Plotting data")
+    fig = px.box(datum, x="cores", y="time", boxmode="group")
 
-      # Use logarithmic y-axis
-      fig.update_yaxes(type="log")
+    # Use logarithmic y-axis
+    fig.update_yaxes(type="log")
 
-      layout = go.Layout(
-          xaxis = dict(
-              title = 'Hardware Threads',
-              tick0 = 1,
-              dtick = 10,
-              range=[0, 41]
-          ),
-          yaxis = dict (
-              title = 'Time Taken (ms)',
-              type="log", tickmode="array", tickvals=[1, 10, 100],
-              title_standoff = 0
-          ),
-          hovermode='closest',
-          legend=dict(
-            orientation="h",
-            yanchor="top",
-            y=0.99,
-            xanchor="right",
-            x=0.995,
-            bgcolor="LightSteelBlue",
-            bordercolor="Black",
-            borderwidth=1,
-            # itemsizing = "constant",
-          ),
-          font=dict(
-            family="Courier New, monospace",
-            size=9,
-            color="black",
-          ),
+    layout = go.Layout(
+        xaxis = dict(
+            title = 'Hardware Threads',
+            tick0 = 1,
+            dtick = 10,
+            range=[0, 41]
+        ),
+        yaxis = dict (
+            title = 'Time Taken (ms)',
+            type="log", tickmode="array", tickvals=[1, 10, 100],
+            title_standoff = 0
+        ),
+        hovermode='closest',
+        legend=dict(
+          orientation="h",
+          yanchor="top",
+          y=0.99,
+          xanchor="right",
+          x=0.995,
+          bgcolor="LightSteelBlue",
+          bordercolor="Black",
+          borderwidth=1,
+          # itemsizing = "constant",
+        ),
+        font=dict(
+          family="Courier New, monospace",
+          size=9,
+          color="black",
+        ),
       )
 
-      fig.update_layout(layout)
-              
-
-      fig.write_html(output_directory + f"/banking_scale.html")
-      fig.write_image(output_directory + f"/banking_scale.pdf")
-      fig.write_image(output_directory + f"/banking_scale.png")
+    fig.update_layout(layout)
+    print("Writing output")
+    fig.write_html(output_directory + f"/banking_scale.html")
+    fig.write_image(output_directory + f"/banking_scale.pdf")
+    fig.write_image(output_directory + f"/banking_scale.png")
